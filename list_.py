9c6b5e9b6ddd0bb05c14314a7160a7a4c350b6da
Syed Abdul Basit AI <syedabdulb15@gmail.com>
2024-10-31 10:39:58 +0500

    Add files via upload

---
+++ list_.py
@@ -0,0 +1,11 @@
+promo=[67,'Basit',6.7,'Karachi']
+print(promo[1],"from",promo[3])
+promo[1]='Abdul Basit'
+print(promo[1],"from",promo[3])
+
+
+
+
+#creating a list number
+list=[10,20,30]
+print(f"The list of numbers are:\n{list}")
