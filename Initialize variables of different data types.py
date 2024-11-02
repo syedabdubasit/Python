a41671024262b287a095bc4d3d3fb1ee038ff289
Syed Abdul Basit AI <syedabdulb15@gmail.com>
2024-11-02 19:12:28 +0500

    Add files via upload

---
+++ Initialize variables of different data types.py
@@ -0,0 +1,12 @@
+integer = 42
+float = 3.14
+complex = 5 + 2j
+
+print("Integer value:", integer)
+print("Type of Integer:", type(integer))
+
+print("\nFloat value:", float)
+print("Type of Float:", type(float))
+
+print("\nComplex value:", complex)
+print("Type of Complex:", type(complex))
